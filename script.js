b16f973253f56551b519f6c0fa23321d69f8fbd4
Chase <Chasequigley9@gmail.com>
2023-09-19 21:56:59 -0400

    further changes

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function textChange(...args) {
     const change = prompt("");
     args.text = change;
 }
+
+
+function swapText() {
+    const element = document.getElementById("typeText")
+    element.innerHTML = "A Student";
+    console.log("working");
+    setTimeout(() => {element.innerHTML = "Chase Quigley"}, 3000);
+}
