30eff295eae0dce6f63af95ef2d46aade62f0653
Charles J Quigley <Chasequigley9@gmail.com>
2024-01-19 18:24:56 -0500

    updates

--- travel_script.js
+++ travel_script.js
@@ -1,7 +1,7 @@
 
 function provideDescription(Int) {
     switch (Int) {
-        
+
         // TO DO:
         // Add descriptions to each individual location
         // Include: Dates, People, Rating / 10 
@@ -10,88 +10,106 @@ function provideDescription(Int) {
         case 1:
             // document.getElementById("photo1").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Andorra la Vella!";
-            document.getElementById("description").innerHTML = "Here I went to Andorra la Vella";
+            document.getElementById("Location").innerHTML = "Andorra la Vella";
+            document.getElementById("description").innerHTML = "Hey! Here my girlfriend Vikki and I took a picture in front of the Gran Valira River Bridge I thought this trip was super fun. I went with my girlfriends family and would for sure go back again!";
             break;
         case 2:
             // document.getElementById("photo2").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Horseshoe Beach, Bermuda!";
+            document.getElementById("Location").innerHTML = "Horseshoe Beach, Bermuda";
+            document.getElementById("description").innerHTML = "Hey! Here my family and I took a trip to the iconic pink sand beach in the sunny island of Bermuda. I thought this place was super relaxing and I loved sitting out in the sun. Bonus points to the cove for the cliff jumping it provided.";
+
             break;
         case 3:
             // document.getElementById("photo3").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "King's Wharf, Bermuda!";
+            document.getElementById("Location").innerHTML = "King's Wharf, Bermuda";
+            document.getElementById("description").innerHTML = "Hey! Here is the port where the cruise docked in Bermuda.";
             break;
         case 4: 
             // document.getElementById("photo4").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Gunstock Mountain Resort, New Hampshire!";
+            document.getElementById("Location").innerHTML = "Gunstock Mountain Resort, New Hampshire";
+            document.getElementById("description").innerHTML = "Hey! Here is a (beautiful) picture of my girlfriend on a black dimaond slope at Gunstock. The lakes in the back give the picture the added boost to an already cool picture.";
             break;
         case 5:
             // document.getElementById("photo5").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "I-95 South, Massachusetts!";
+            document.getElementById("Location").innerHTML = "I-95 South, Massachusetts";
+            document.getElementById("description").innerHTML = "Hey! Here is a picture I thought looked cool as I was driving south to Rhode Island. The mixture of the cloudy blue sky and the mismatched tree colors give a perfect depiction of a massachusetts fall season.";
             break;
         case 6: 
             // document.getElementById("photo6").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Kangamangus Hiking Trail, New Hampshire!";
+            document.getElementById("Location").innerHTML = "Kangamangus Hiking Trail, New Hampshire";
+            document.getElementById("description").innerHTML = "Hey! Here is a picture of nature I took on a walk throughout the amazing forests of New Hampshire. I love to see snow within nature and so this is simply an extention of that.";
             break;
         case 7:
             // document.getElementById("photo7").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "New York City, New York!";
+            document.getElementById("Location").innerHTML = "New York City, New York";
+            document.getElementById("description").innerHTML = "Hey! This a special picture to me as it is the first time I have been in New York City (and furthermore, New York itself). I really enjoying exploring the streets and everything the city had to offer.";
             break;
         case 8: 
             // document.getElementById("photo8").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "The Ocean, Aruba!";
+            document.getElementById("Location").innerHTML = "The Ocean, Aruba";
+            document.getElementById("description").innerHTML = "Hey! Here is a super pretty picture of the sun setting above the ocean horizon in Aruba. This was seen on a pirate (booze cruise) ship which added to the enjoyment and atmospheremic happiness"; 
+
             break;
         case 9:
             // document.getElementById("photo9").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Attitash Mountain Resort, New Hampshire!";
+            document.getElementById("Location").innerHTML = "Attitash Mountain Resort, New Hampshire";
+            document.getElementById("description").innerHTML = "Hey! This is one of my favorite pictures. Recently I have fallen more in love with hitting the mountain resorts and skiing (although I have switched to the dark side of snowboarding). This picture captures that excitement and happiness I have when I hit the resorts and so it holds a special place to me.";
             break;
         case 10: 
             // document.getElementById("photo10").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "The Tundra, Iceland!";
+            document.getElementById("Location").innerHTML = "The Tundra, Iceland";
+            document.getElementById("description").innerHTML = "Hey! Here is a SUPER interesting picture to me because it captures the vast emptiness of Iceland. I am also partial to this picture because I also drive a Subaru Crosstrek at home as well and so this was cool to see it out in action in Iceland as well.";
             break;
         case 11:
             // document.getElementById("photo11").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Vik Beach, Iceland!";
+            document.getElementById("Location").innerHTML = "Vik Beach, Iceland";
+            document.getElementById("description").innerHTML = "Hey! Here is an AWESOME picture of the black sand beach in the town of Vik. I like this picture a lot also because my girlfriend's name is Vik, and I think that's cool.";
             break;
         case 12: 
             // document.getElementById("photo12").src = "Travel_Photos/Green.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "TD Garden, Massachusetts!";
+            document.getElementById("Location").innerHTML = "TD Garden, Massachusetts";
+            document.getElementById("description").innerHTML = "Hey! Here is my girlfriend and I at a (preseason) Bruins game. This picture looks awesome and I was honestly suprised my camera was able to take such a nice picture.";
             break;
         case 13:
             // document.getElementById("photo13").src = "Travel_Photos/Ball.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Big Chair Aruba, Aruba!";
+            document.getElementById("Location").innerHTML = "Big Chair Aruba, Aruba";
+            document.getElementById("description").innerHTML = "Hey! Exactly as it sounds, this is my girlfriend and I in a really big chair in Aruba. An awesome landmark in the small little island it holds a special place to me in memory of the trip we took.";
             break;
         case 14: 
             //  document.getElementById("photo14").src = "Travel_Photos/Ball.HEIC";
              document.getElementById("textContainer").style.display = "block";
-             document.getElementById("Location").innerHTML = "Reykjavik, Iceland!";
+             document.getElementById("Location").innerHTML = "Reykjavik, Iceland";
+             document.getElementById("description").innerHTML = "Hey! This is a cool little picture of a Christmas tree in the capitol Reykjavik. I thought it looked really pretty and reflected off the building nicely with the lights";
             break;
         case 15:
             // document.getElementById("photo15").src = "Travel_Photos/Ball.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Northern Lights, Iceland!";
+            document.getElementById("Location").innerHTML = "Northern Lights, Iceland";
+            document.getElementById("description").innerHTML = "Hey! Here is a SUPER cool picture of the Nothern Lights over in Iceland. This was the first time I have ever seen the lights before and this was definitley an awesome first experience.";
             break;
         case 16: 
             // document.getElementById("photo16").src = "Travel_Photos/Ball.HEIC";
             document.getElementById("textContainer").style.display = "block";
-            document.getElementById("Location").innerHTML = "Portsmouth, New Hampshire!";
+            document.getElementById("Location").innerHTML = "Portsmouth, New Hampshire";
+            document.getElementById("description").innerHTML = "Hey! Here is a nice little picture of my girlfriend and I over in Portsmouth. I thought this place was really nice and enjoyable to walk around in.";
             break;
         case 17:
             // document.getElementById("photo17").src = "Travel_Photos/Ball.HEIC";
             document.getElementById("textContainer").style.display = "block";
             document.getElementById("Location").innerHTML = "The SOOOB, Pennsylvania";
+            document.getElementById("description").innerHTML = "Hey! Here is my dog Scout, in the car on his way back from Pennsylvania. My mom and I drove to pick him up (a VERY long drive). Thankfully he stayed sleeping most of the car ride home and he was able to make it to his permenant new home back in Boston. We drove him in the Subaru, of course (The SOOOOOOOB).";
         default:
             break;
     }
