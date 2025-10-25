6adbc824d12da30fecbe3df6716aac8d0e84e51a
Chase Quigley <chasequigley9@gmail.com>
2025-10-25 13:36:05 -0400

    push to development branch

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+let i = 0;
 
+window.onload = () => {
+    var image = document.getElementById("pfp");
+    let images = ["Assets/ice_castles.jpg", "Assets/factorio.png"];
+
+    image_swap = () => {
+        image.style.transform = 'rotateY(90deg)';
+        setTimeout(() => {
+            i = (i + 1) % images.length;
+            image.src = images[i];
+            image.style.transform = 'rotateY(0deg)'; 
+        }, 300);
+    }
+    setInterval(image_swap, 5000);
 }
